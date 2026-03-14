a22f4b858403beae9eb09a0119642dd1f1e62bb1
The3Arrow <jasiu1337@gmail.com>
2026-03-14 17:40:54 +0100

    Done lab2 and lab3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "pl.wsei.pam.lab01"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "pl.wsei.pam.lab01"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -40,6 +40,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.13.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/lab02/Lab02Activity.kt
+++ app/src/main/java/lab02/Lab02Activity.kt
@@ -1,6 +1,5 @@
-package pl.wsei.pam.lab01
+package lab02
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -8,25 +7,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import lab02.Lab02Activity
+import pl.wsei.pam.lab01.R
 
-class MainActivity : AppCompatActivity() {
+class Lab02Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lab02)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-    fun onClickMainBtnRunLab01(v: View){
-        val intent = Intent(this, Lab01Activity::class.java)
-        startActivity(intent)
-    }
-    fun onClickMainBtnRunLab02(v: View){
-        val intent = Intent(this, Lab02Activity::class.java)
-        startActivity(intent)
+    fun onBoardSizeClick(view: View) {
+        // Rzutowanie tagu na String
+        val tagString = view.tag as String
+
+        val dimensions = tagString.split(" ")
+
+        if (dimensions.size == 2) {
+            val columns = dimensions[0].toInt()
+            val rows = dimensions[1].toInt()
+
+            Toast.makeText(this, "Wybrano planszę: $columns kolumn x $rows wierszy", Toast.LENGTH_SHORT).show()
+        }
     }
 }
